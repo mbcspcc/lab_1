7e80ca470df64c352f35913d479b730ff026c6ca
mbcspcc <165945148+mbcspcc@users.noreply.github.com>
2024-04-07 17:09:07 -0700

    Changes v test debug correct
    Changes to correct return, calculate tax rate * total cost, change var name for consistency

--- lab1.py
+++ lab1.py
@@ -1,17 +1,39 @@
+#*****************************************************************************
+# Author:          Margret Bailey G04288409
+# Assignment:      CIS-133Y 2024 Spring Lab 1
+# Date:            04/07/24
+# Description:     A test/debug/refactor of a program designed to calculate
+#                    total costs (item cost(s), calculated tax, shipping)
+#                    and display the results to screen with total remit amt
+# Input:           4 inputs (1 repeating); itemCost (float - 1-n entries),
+#                    more (string) - asks user whether there are more items,
+#                    taxRate (float), shippingCost (float)
+# Output:          totalCost (float - sum of itemCost entries),
+#                    taxRate * totalCost (float), shippingCost (float),
+#                    remittance line (float); various string wrapper
+# Sources:         Class materials
+#*****************************************************************************
+
+
+
 # Shopping program
 # Inputs: float cost, float taxRate, float shipping
 # Outputs: float totalPrice
 
 def main():
     taxRate = 0.0
-    shipping = 0.0
+    shippingCost = 0.0
     totalCost = 0.0
 
     totalCost = getItemCosts()
-    taxRate, shipping = getOtherCosts()
-    printReceipt(totalCost, taxRate, shipping)
+    taxRate, shippingCost = getOtherCosts()
+    printReceipt(totalCost, taxRate, shippingCost)
 
     print("Thank you!")
+#    print(totalCost)
+#    print(taxRate)
+#    print(totalCost * taxRate)
+#    print(shippingCost)
 
 # getItemCosts() prompts a user for a list of item
 # costs and returns the sum
@@ -21,31 +43,31 @@ def getItemCosts():
     more = 'y'
 
     while more == 'y':
-        itemcost = float(input("Enter item cost $ "))
+        itemCost = float(input("Enter item cost $ "))
         totalCost = totalCost + itemCost
         more = input("Do you have more items (y/n): ")
 
     return totalCost
- 
+
 # getOtherCosts() prompts a user for the tax rate
 # and shipping costs and returns both inputs
 def getOtherCosts():
     taxRate = 0.0
-    shipping = 0.0
+    shippingCost = 0.0
 
     taxRate = float(input("\nEnter tax rate (i.e. .075 for 7.5%): "))
-    shipping = float(input("Enter shipping costs $ "))
+    shippingCost = float(input("Enter shipping costs $ "))
 
-    return shipping, taxRate
+    return taxRate, shippingCost
 
-# printReceipt() accepts total cost, tax rate, and 
+# printReceipt() accepts total cost, tax rate, and
 # shipping costs and calculates and prints the tax 
 # amount, and total cost
-def printReceipt(totalCost, taxRate, shipping):
+def printReceipt(totalCost, taxRate, shippingCost):
     print("\nSubtotal: $ ", format(totalCost, ".2f"))
-    print("Tax: $", format((taxRate), ".2f"))
-    print("Shipping: $", format(shipping,".2f"))
+    print("Tax: $", format((taxRate * totalCost), ".2f"))
+    print("Shipping: $", format(shippingCost,".2f"))
     print("------------------------")
-    print("Please pay: $", format(totalCost + taxRate + shipping, ".2f"))
+    print("Please pay: $", format(totalCost + (taxRate * totalCost) + shippingCost, ".2f"))
 
 main()
